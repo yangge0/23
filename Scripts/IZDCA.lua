1a9f86061d30d1bef7c41145e4977891a5ae745b
lilong <d7xxhsrx2w@privaterelay.appleid.com>
2025-12-19 13:06:31 +0800

    Add files via upload

---
+++ Scripts/IZDCA.lua
@@ -0,0 +1,23 @@
+﻿local JD = GameMain:GetMod("IZDCA");--先注册一个新的MOD模块
+
+function JD:Hension()
+local Npc = me.npcObj
+if  Npc.MaxLing >= 1 then
+local Practice = Npc.PropertyMgr.Practice
+local Time = Npc.MaxLing/Practice:BaseAbsorbEffectGold()
+local Value = Practice:GoldEffectSpeed(Npc.Key)
+Practice:MakeGold(Time * Value)
+Npc:AddLing(-Npc.MaxLing)
+
+world:ShowStoryBox("恭喜"..Npc.Name.."修為更上一層樓。","修為精進",{"知道了"},
+function(key)
+if key == 0 then
+Npc.LuaHelper:AddMsg(""..Npc.Name.."获得了")
+end
+end
+)
+
+end
+
+end
+
